b4401009afab84b669ab15ed32d50638863e6054
Abhi1205 <abhinavre58@gmail.com>
2026-03-08 21:23:57 +0530

    added vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 createRoot(document.getElementById("root")!).render(<App />);
